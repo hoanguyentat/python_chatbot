61fd648a72197d42087b98ec87486ffc6e6b7931
Hola Nguyen <Hola Nguyen>
2017-01-13 09:27:39 +0700

    add chatbot

--- chatbot/echobot.py
+++ chatbot/echobot.py
@@ -1,11 +1,10 @@
 import json
 import requests
-from parse import *
+import urllib
+import time
 
 TOKEN = "324199340:AAGcvFLAuMd8adm2snY3HGO4NUVecZzLmHI"
 URL = "https://api.telegram.org/bot{}/".format(TOKEN)   #insert token to the url
-print URL
-# print "\n"
 
 def get_url(url):
 	res = requests.get(url)
@@ -15,10 +14,24 @@ def get_json_from_url(url):
 	content = get_url(url)
 	js_content = json.loads(content)
 	return js_content
-def get_updates():
+def get_updates(offset=None):
 	url = URL + "getUpdates"
+	if offset:
+		url += "?offset={}&timeout=100".format(offset)
 	js_content = get_json_from_url(url)
 	return js_content
+
+def get_last_update_id(updates):
+	update_ids = []
+	for update in updates["result"]:
+		update_ids.append(int(update["update_id"]))
+	return max(update_ids)		#return the biggest id in the conversation
+
+def echo_all(updates):
+	for update in updates["result"]:
+		text = update["message"]["text"]
+		chat_id = update["message"]["chat"]["id"]
+		send_message(text, chat_id)
 
 def get_last_chat_id_and_text(updates):
 	num_updates = len(updates["result"])
@@ -26,16 +39,21 @@ def get_last_chat_id_and_text(updates):
 	text = updates["result"][last_update]["message"]["text"]
 	chat_id = updates["result"][last_update]["message"]["chat"]["id"]
 	return(text, chat_id)
+
 def send_message(text, chat_id):
+	# text = parse.quote_plus(text) 			#correctly encoding our message text
+	text = urllib.quote_plus(text.encode('utf-8'))
 	url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
-	# url = parse("sendMessage?text={}&chat_id={}",text,chat_id)
-	print url
-	# url = URL + "sendMessage?text=" + text + "&chat_id=" + chat_id	
 	get_url(url)
 
-text, chat = get_last_chat_id_and_text(get_updates())
-# text = text.decode('utf-8', 'ignore')
-print text
-text = text.encode('ascii', 'ignore')     #Vietnamese to reply
-print text, chat
-send_message(text, chat)
+def main():
+	last_update_id = None
+	while True:
+		updates = get_updates(last_update_id)
+		if (len(updates["result"]) > 0):
+			last_update_id = get_last_update_id(updates) + 1
+			print last_update_id
+			echo_all(updates)
+		time.sleep(0.5)
+if __name__ == '__main__':
+	main()
